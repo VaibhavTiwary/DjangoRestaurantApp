96f515696a1ea78121289bd7d2b24aa12103d94d
Vaibhav <vaibhavtiwary7766@gmail.com>
2024-04-12 19:30:31 +0530

    Cart feature

--- restr/models.py
+++ restr/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -9,3 +10,13 @@ class Menu(models.Model):
 
     def __str__(self):
         return self.items
+
+
+class CartItem(models.Model):
+    product = models.ForeignKey(Menu, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=0)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.quantity} x {self.product.items}"
--- restr/views.py
+++ restr/views.py
@@ -7,13 +7,42 @@ from .forms import CreateUserForm
 from django.contrib.auth import authenticate, login, logout
 from .decorators import unauthenticated_user
 from django.contrib.auth.models import Group
+from .models import Menu, CartItem
 
 
 # Create your views here.
 
 
 def index(request):
-    return render(request, "restr/index.html")
+    menus = Menu.objects.all()
+    return render(request, "restr/index.html", {"menus": menus})
+
+
+def view_cart(request):
+    cart_items = CartItem.objects.filter(user=request.user)
+    # price = (item.product.price * item.quantity for item in cart_items)
+    total_price = sum(item.product.price * item.quantity for item in cart_items)
+    return render(
+        request,
+        "restr/cart.html",
+        {"cart_items": cart_items, "total_price": total_price},
+    )
+
+
+def add_to_cart(request, product_id):
+    product = Menu.objects.get(id=product_id)
+    cart_item, created = CartItem.objects.get_or_create(
+        product=product, user=request.user
+    )
+    cart_item.quantity += 1
+    cart_item.save()
+    return redirect("restr:view_cart")
+
+
+def remove_from_cart(request, item_id):
+    cart_item = CartItem.objects.get(id=item_id)
+    cart_item.delete()
+    return redirect("restr:view_cart")
 
 
 @unauthenticated_user
@@ -32,7 +61,7 @@ def registerPage(request):
             messages.success(
                 request, "Account created successfully for " + user.username
             )
-            return HttpResponseRedirect(reverse("restr:ownerLogin"))
+            return HttpResponseRedirect(reverse("restr:login"))
 
     context = {"form": form}
 
